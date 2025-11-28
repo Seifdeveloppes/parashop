97e172ae93d765e37bb496867e1f45de389b0a86
beautydecodedparashop <stebeautydecodedparashop@outlook.com>
2025-11-28 17:59:52 +0100

    Suppression importmap et fix path index

--- vite.confg.ts
+++ vite.confg.ts
@@ -4,13 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Cette ligne permet d'injecter la clé API lors du build Vercel
+    // Injecte la variable d'environnement lors du build
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
-    // Cette ligne évite les crashs "process is not defined" dans le navigateur
+    // Évite le crash "process is not defined" dans le navigateur
     'process.env': {}
   },
   build: {
-    // Assure que le dossier de sortie est standard
     outDir: 'dist',
   }
 });
