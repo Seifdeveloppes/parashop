b91ade6b4765e5f774efd7bf13ae80cca558f9a9
beautydecodedparashop <stebeautydecodedparashop@outlook.com>
2025-11-28 16:07:57 +0100

    Correction finale index.html et build

--- vite.confg.ts
+++ vite.confg.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Safely expose specific environment variables to the client
-    'process.env': {
-      API_KEY: process.env.API_KEY || ''
-    }
+    // Cette ligne permet d'injecter la clé API lors du build Vercel
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY),
+    // Cette ligne évite les crashs "process is not defined" dans le navigateur
+    'process.env': {}
   },
 });
